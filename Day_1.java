848d40f0605d63861d802f824d5cd59ab1e3c779
Joydeep Pradhan <122430428+joydeep-pradhan09@users.noreply.github.com>
2024-06-02 11:17:58 +0530

    Add files via upload

---
+++ Day_1.java
@@ -0,0 +1,7 @@
+public class Day_1 {
+    // This is the main method, the entry point of any Java application
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!"); // 🌟
+    }
+}
