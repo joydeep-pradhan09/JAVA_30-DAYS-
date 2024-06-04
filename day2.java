3c2799dc0d41eeffc447b56ea58aa10f9ec233c0
Joydeep Pradhan <122430428+joydeep-pradhan09@users.noreply.github.com>
2024-06-04 13:48:02 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,10 @@
+//**Day 2:** Sum of two numbers.
+public class day2 {
+    public static void main(String[] args) {
+        int num1;//variable declaration
+        num1=5;//variable initialization
+        int num2=10; // both in one line
+        int sum=num1+num2; // store sum
+        System.out.println("sum ="+sum); //print value of sum    
+    }
+}
